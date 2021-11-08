9d6a0be4c8abc7edca49e06ce8604c2ffb7ec68e
crekulon <czevanoi@gmail.com>
2021-11-07 19:58:58 -0600

    set up media fields for a new media section on the homepage

--- home.js
+++ home.js
@@ -19,11 +19,8 @@ onLoaded(() => {
 })
 
 //when people click the highlight selector, swap the image and text being shown by toggling the highlighted class
-let newStuffSelectors = document.querySelectorAll("#newstuff .selector")
-for (let i = 0; i < newStuffSelectors.length; i++) {
-    const selector = newStuffSelectors[i];
-
-    selector.addEventListener('mousedown', function(){
+document.querySelectorAll("#newstuff .selector").forEach(selector => {
+    selector.addEventListener('click', function(){
         let oldSelect = document.querySelector('#newstuff .selector.selected')
         if(oldSelect) {
             oldSelect.classList.remove('selected')
@@ -36,12 +33,10 @@ for (let i = 0; i < newStuffSelectors.length; i++) {
         selector.classList.add('selected')
         document.querySelectorAll(`.highlight-${selector.getAttribute('highlights')}`).forEach(e => e.classList.add('highlighted'))
     })			
-}
+})
 
-let accountSelectors = document.querySelectorAll(".accountstuff .selector")
-for (let i = 0; i < accountSelectors.length; i++) {
-    const selector = accountSelectors[i];
-    selector.addEventListener('mousedown', function(){
+document.querySelectorAll(".accountstuff .selector").forEach(selector => {
+    selector.addEventListener('click', function(){
         let oldSelect = document.querySelector('.accountstuff .selector.selected')
         if(oldSelect) {
             oldSelect.classList.remove('selected')
@@ -51,4 +46,29 @@ for (let i = 0; i < accountSelectors.length; i++) {
         selector.classList.add('selected')
         selector.nextElementSibling.classList.add('active')
     })			
-}
+})
+
+//video overlay + opener for media
+document.querySelectorAll('.vid-opener').forEach(e => {
+    e.style.backgroundImage = `url(https://i.ytimg.com/vi/${e.getAttribute('data-vid')}/maxresdefault.jpg)`
+
+    e.addEventListener('click', function(){
+        document.body.classList.add('open-vid')
+        document.querySelector('.video-container iframe').src = `https://www.youtube.com/embed/${e.getAttribute('data-vid')}`
+    })
+})
+
+document.querySelectorAll('.image-opener').forEach(e => {
+    e.addEventListener('click', function(){
+        document.body.classList.add('open-img')
+        document.querySelector('.gallery-container .gallery-image').src = e.style.backgroundImage.replace('url(', '').replace(')', '').replaceAll('"', '')
+    })
+})
+
+document.querySelectorAll('.media-bg, .media-close').forEach(e => {
+    e.addEventListener('click', ()=> {
+        document.body.classList.remove('open-vid')
+        document.body.classList.remove('open-img')
+        document.querySelector('.video-container iframe').src = ""
+    })
+})
